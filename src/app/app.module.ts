1bea7e3060f00a6086891e4e4741749238ff6d5d
unknown <frevill02@gmail.com>
2024-11-22 15:34:12 -0500

    configuracion variables de entorno con firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"examenbimestral-55713","appId":"1:426699423403:web:b2917fff3cc3be66f241ee","storageBucket":"examenbimestral-55713.firebasestorage.app","apiKey":"AIzaSyBUeD3svFSpZ-z3gO18iPRx5TFDXcydZjk","authDomain":"examenbimestral-55713.firebaseapp.com","messagingSenderId":"426699423403","measurementId":"G-WWZRSGBZ9M"})), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
